d097d24491316b9868f80d659ba26acffff9d47d
benjaminm206 <benjaminm206@nycstudents.net>
2025-02-05 09:24:53 -0500

    Data-Types

--- app.py
+++ app.py
@@ -2,9 +2,25 @@ x = 3
 y = float(3)
 print(x,y)
 
-""" values = [1,2.23,5,7,2,30,15]
+values = [1,2.23,5,7,2,30,15]
 print(values)
 for i in values:
-    print(i) """
+    print(i)
 
-    
+print(values[0])
+print(values[6])
+
+# Challenge notes:
+# I will create an input where the terminal will ask a question
+# When the user responds, it will count the number of words in the string
+# display the number it recieves
+x = input("What month are we in?")
+y= x.split( )
+z = y[0]
+print(y)
+print(z)
+count = len(y)
+print(count)
+
+""" time_of_day = input("What month is it?") 
+ """
---
+++ madlibs.py
@@ -0,0 +1,3 @@
+x = input("Insert a verb in the present tense (example: running)")
+y = input("Insert another verb!")
+z = input("Insert a noun (It could be a food, an object, anything!)")
